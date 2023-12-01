3759ac1d613bf7a331b052516f332379c583554d
Martijn Kunstman <martijn@kunstman.nl>
2023-12-01 23:07:18 +0100

    clean

--- day1/script.js
+++ day1/script.js
@@ -1,15 +1,15 @@
 fetch("./day1/input.txt").then((r) => r.text()).then((d) => {
   result(d)
-  sInts = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
-  sInts.map((a, i) => d = d.replaceAll(a, a[0]+(i + 1)+a.slice(-1)))
+  n = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+  n.map((a, i) => d = d.replaceAll(a, a[0]+(i + 1)+a.slice(-1)))
   result(d)
 })
 result = (d) => {
-  ans = 0
+  r = 0
   d.split("-").map((a) => {
-    first = a.split('').find((b) => !isNaN(b))
-    last = a.split('').findLast((b) => !isNaN(b))
-    ans += Number(first + "" + last)
+    f = a.split('').find((b) => !isNaN(b))
+    l = a.split('').findLast((b) => !isNaN(b))
+    r += Number(f + "" + l)
   })
-  document.getElementById("answer").innerHTML += ans + " "
+  document.querySelector("#answer").innerHTML += r + " "
 }
