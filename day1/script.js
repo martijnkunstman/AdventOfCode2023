457f7a7de5e8febebf972e6aa2ab16d82e7a52df
Martijn Kunstman <martijn@kunstman.nl>
2023-12-01 22:27:28 +0100

    clean

--- day1/script.js
+++ day1/script.js
@@ -1,21 +1,17 @@
-fetch("./day1/input1.txt")
-  .then((r) => r.text())
-  .then((d) => {
-    result(d);
-  });
-fetch("./day1/input2.txt")
-  .then((r) => r.text())
-  .then((d) => {
-    let replaceNumbers = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
-    replaceNumbers.map((a, i) => d = d.replaceAll(a, a[0]+(i + 1)+a.slice(-1)));
-    result(d);
-  });
+fetch("./day1/input1.txt").then((r) => r.text()).then((d) => {
+  result(d)
+})
+fetch("./day1/input2.txt").then((r) => r.text()).then((d) => {
+  replaceNumbers = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+  replaceNumbers.map((a, i) => d = d.replaceAll(a, a[0]+(i + 1)+a.slice(-1)))
+  result(d)
+})
 let result = (input) => {
-  let answer = 0;
+  answer = 0
   input.split("-").map((a) => {
-    let first = a.split('').find((b) => !isNaN(b));
-    let last = a.split('').findLast((b) => !isNaN(b));
-    answer += Number(first + "" + last);
-  });
-  document.getElementById("answer").innerHTML += answer + "<br>";
-};
+    first = a.split('').find((b) => !isNaN(b))
+    last = a.split('').findLast((b) => !isNaN(b))
+    answer += Number(first + "" + last)
+  })
+  document.getElementById("answer").innerHTML += answer + " "
+}
