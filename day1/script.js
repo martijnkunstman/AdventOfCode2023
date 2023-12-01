3007b10533bde55cbe90cd6fce9aa9533edb415b
Martijn Kunstman <martijn@kunstman.nl>
2023-12-01 22:35:08 +0100

    clean2

--- day1/script.js
+++ day1/script.js
@@ -2,16 +2,16 @@ fetch("./day1/input1.txt").then((r) => r.text()).then((d) => {
   result(d)
 })
 fetch("./day1/input2.txt").then((r) => r.text()).then((d) => {
-  replaceNumbers = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
-  replaceNumbers.map((a, i) => d = d.replaceAll(a, a[0]+(i + 1)+a.slice(-1)))
+  sInts = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+  sInts.map((a, i) => d = d.replaceAll(a, a[0]+(i + 1)+a.slice(-1)))
   result(d)
 })
 let result = (d) => {
-  answer = 0
+  ans = 0
   d.split("-").map((a) => {
     first = a.split('').find((b) => !isNaN(b))
     last = a.split('').findLast((b) => !isNaN(b))
-    answer += Number(first + "" + last)
+    ans += Number(first + "" + last)
   })
-  document.getElementById("answer").innerHTML += answer + " "
+  document.getElementById("answer").innerHTML += ans + " "
 }
