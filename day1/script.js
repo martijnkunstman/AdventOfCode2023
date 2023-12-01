7c50bdfec385d2c529b5629a8e0b42be35f05b6d
Martijn Kunstman <martijn@kunstman.nl>
2023-12-01 22:02:06 +0100

    nice

--- day1/script.js
+++ day1/script.js
@@ -6,15 +6,8 @@ fetch("./day1/input1.txt")
 fetch("./day1/input2.txt")
   .then((r) => r.text())
   .then((d) => {
-    d = d.replaceAll("one", "o1e");
-    d = d.replaceAll("two", "t2o");
-    d = d.replaceAll("three", "t3e");
-    d = d.replaceAll("four", "f4r");
-    d = d.replaceAll("five", "f5e");
-    d = d.replaceAll("six", "s6x");
-    d = d.replaceAll("seven", "s7n");
-    d = d.replaceAll("eight", "e8t");
-    d = d.replaceAll("nine", "n9e");
+    let replaceNumbers = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+    replaceNumbers.map((a, i) => d = d.replaceAll(a, a[0]+(i + 1)+a.slice(-1)));
     result(d);
   });
 let result = (input) => {
--- script.js
+++ script.js
@@ -1,7 +1,16 @@
+let day = 1;
+
 for (let i = 1; i < 26; i++) {
+  if (i <= day) {
   document.getElementById("buttons").innerHTML +=
     "<button onclick='showDay(" + i + ")'>day " + i + "</button>";
+  }
+  else {
+    document.getElementById("buttons").innerHTML +=
+    "<button disabled style='opacity:0.50;'>day " + i + "</button>";
+  }
 }
+showDay(day);
 function showDay(day) {
   document.getElementById("answer").innerHTML = "";
   document.getElementById("code").innerHTML = '<pre><code id="jscode" class="language-javascript"></code></pre>';
