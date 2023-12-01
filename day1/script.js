682317240cb0bbead9982b439edcc642edbc7ec6
Martijn Kunstman <martijn@kunstman.nl>
2023-12-01 22:28:26 +0100

    d

--- day1/script.js
+++ day1/script.js
@@ -6,9 +6,9 @@ fetch("./day1/input2.txt").then((r) => r.text()).then((d) => {
   replaceNumbers.map((a, i) => d = d.replaceAll(a, a[0]+(i + 1)+a.slice(-1)))
   result(d)
 })
-let result = (input) => {
+let result = (d) => {
   answer = 0
-  input.split("-").map((a) => {
+  d.split("-").map((a) => {
     first = a.split('').find((b) => !isNaN(b))
     last = a.split('').findLast((b) => !isNaN(b))
     answer += Number(first + "" + last)
