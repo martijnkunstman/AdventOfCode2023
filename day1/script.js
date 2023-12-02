b55144c585e086c4a02665add25986bf47a3cdea
Martijn Kunstman <martijn@kunstman.nl>
2023-12-02 01:05:18 +0100

    result = r

--- day1/script.js
+++ day1/script.js
@@ -1,15 +1,13 @@
 fetch("./day1/input.txt").then((r) => r.text()).then((d) => {
-  result(d)
+  c(d)
   n = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
   n.map((a, i) => d = d.replaceAll(a, a[0]+(i + 1)+a.slice(-1)))
-  result(d)
+  c(d)
 })
-result = (d) => {
+c = (d) => {
   r = 0
   d.split("-").map((a) => {
-    f = a.match(/\d/);
-    l = a.match(/\d(?=\D*$)/);
-    r += f*10 + l*1
+    r += a.match(/\d/)*10 + a.match(/\d(?=\D*$)/)*1
   })
   document.querySelector("#answer").innerHTML += r + " "
 }
