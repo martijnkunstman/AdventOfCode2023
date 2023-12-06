612edef365837dfd648be2934f46bb5c47901d55
Martijn Kunstman <martijn@kunstman.nl>
2023-12-06 20:41:35 +0100

    day4

--- day4/script.js
+++ day4/script.js
@@ -1,8 +1,13 @@
 let data = [];
+let cardPoints = [];
+let cardPoints2 = [];
 let answer1 = 0;
 let answer2 = 0;
+
+let counter2 = 0;
+
 fetch("input.txt")
-//fetch("input1_test.txt")
+  //fetch("input1_test.txt")
   .then((r) => r.text())
   .then((d) => {
     d = d.replaceAll("  ", " ");
@@ -20,7 +25,7 @@ fetch("input.txt")
     //part 1
     for (let i = 0; i < data.length; i++) {
       let count = 0;
-      console.log(data[i]);
+      //console.log(data[i]);
       let found = 0;
 
       for (let j = 0; j < data[i][0].length; j++) {
@@ -32,14 +37,32 @@ fetch("input.txt")
       }
 
       if (found != 0) {
-        console.log("found:" + 2 ** (found-1));
-        answer1 += 2 ** (found-1);
+        //console.log("found:" + 2 ** (found-1));
+        answer1 += 2 ** (found - 1);
+        cardPoints.push(found);
+        cardPoints2.push(0);
+      } else {
+        cardPoints.push(0);
+        cardPoints2.push(0);
       }
     }
+
+    //part 2 - from cardPoints
+    console.log(cardPoints);
+    counter2 = 0;
+
+    for (let i = 0; i < cardPoints2.length; i++) {
+      cardPoints2[i]++;
+      for (let j = 0; j < cardPoints[i]; j++) {
+        for (let k = 0; k < cardPoints2[i]; k++) {
+          cardPoints2[j + i + 1]++;
+        }
+      }
+    }
+
+    console.log(cardPoints2);
     console.log(answer1);
-
+    console.log(cardPoints2.reduce((a, b) => a + b));
     //
-    console.log(data);
+    //console.log(data);
   });
-
-//data
